ee5d17331357b712a11745a53301a7778dab6d8d
vladislavkhvostov <vladkhvo@yandex.ru>
2025-01-14 01:24:38 +0500

    test

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,21 @@
-import { Controller, Post } from '@nestjs/common';
+import { Controller, Post, Req, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import { json, send } from 'micro';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Post()
-  getHello() {
-    return this.appService.getHello();
+  async getHello(@Req() req: any, @Res() res: Response) {
+    console.log(req, res)
+    return {
+      version: 1,
+      session: req.session,
+      response: {
+        text: 'test',
+        end_session: req.end_session
+      }
+    }
   }
 }
